8a2ddb7d267062aa6f418828ebfb99a870af0240
liaoxuezhi <liaoxuezhi@baidu.com>
2014-02-27 16:04:01 +0800

    fix npm install bug

--- build.js
+++ build.js
@@ -1,41 +1,13 @@
-var binding, SASS_OUTPUT_STYLE, path;
+// simply try the test script.
 
-var map = require('./lib/map.json'),
-    candidates, version, modPath, candidate;
+try {
+    require('./test.js');
+} catch( e ) {
 
-version = process.versions.node;
-candidates = map[process.platform + '-' + process.arch] || [];
+    console.log( e.message );
+    console.error('Install fail');
 
-function greaterThanOrEqual(a, b) {
-    return parseInt(a.replace(/\./g, '00')) >=
-        parseInt(b.replace(/\./g, '00'));
-}
-
-if (candidates.length) {
-    do {
-        candidate = candidates.pop();
-
-        if (greaterThanOrEqual(version, candidate)) {
-            break;
-        }
-
-    } while (candidates.length);
-
-    modPath = './lib/' + process.platform + '-' + process.arch + '/' +
-        candidate + '/binding.node';
-} else {
-    console.error( 'Install failed!' );
     process.exit(1);
 }
 
-binding = require(modPath);
 
-
-var source = 'ul{li{color:white;}}',
-    expect = /^ul\s*li\s*{/i;
-
-if ( !binding.renderSync(source).match( expect ) ) {
-    throw new Error( 'Some thing wrong.' );
-} else {
-    console.log( 'Simple test passed!');
-}
