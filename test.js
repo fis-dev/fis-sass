f763c93ecec5c418dd8e6f0f298fe88140872b8b
2betop <2betop.cn@gmail.com>
2015-01-27 13:36:48 +0800

    update

--- test.js
+++ test.js
@@ -2,20 +2,12 @@
 
 var source = 'ul{li{color:white;}}',
     expect = /^ul\s*li\s*{/i,
-    sass = require('./index.js');
+    sass = require('./lib/index.js');
 
-if ( !sass.renderSync({ data: source }).match( expect ) ) {
+var ret = sass.renderSync({ file: 'xxx.scss', data: source });
+
+if ( !ret.css.match( expect ) ) {
     throw new Error( 'Some thing wrong.' );
 } else {
     console.log( 'Simple test passed!');
-}
-
-
-var fs = require('fs');
-source = fs.readFileSync('test.sass', 'utf-8');
-expect = fs.readFileSync('expect.css', 'utf-8');
-if ( sass.renderSync({ data: source, sassSyntax: true }) === expect ) {
-    console.log( 'Sass syntax supported.');
-} else {
-    console.log( 'Sass syntax unsupported.');
 }
